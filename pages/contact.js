85a7dafadb4ff7f02bb765eb8a227a36186c7866
GargDivanshu <divanshu.garg113@gmail.com>
2023-03-26 18:05:21 +0530

    redo front

--- pages/contact.js
+++ pages/contact.js
@@ -6,9 +6,9 @@ const Contact = () => {
       <div className="flex flex-col pt-12 justify-center">
         <p className="stencil text-4xl text-[white] pb-8">Contact Us</p>
         <p className="font-inter text-lg font-semibold pb-20 text-white">
-          <span class="my-4 stencil text-3xl text-black ">Aditya </span> - 90575 41946, ady1812sha@gmail.com
+          <span className="my-4 stencil text-3xl text-black ">Aditya </span> - 90575 41946, ady1812sha@gmail.com
           <br></br>
-          <span class="my-4 stencil text-3xl text-black ">Deepak </span> - 82399 84913, deepak.cse.dtu@gmail.com
+          <span className="my-4 stencil text-3xl text-black ">Deepak </span> - 82399 84913, deepak.cse.dtu@gmail.com
         </p>
 
       </div>
