f135417adf8784637ad56968e1055743101188cc
GargDivanshu <divanshu.garg113@gmail.com>
2023-03-28 00:42:56 +0530

    minor changes

--- pages/contact.js
+++ pages/contact.js
@@ -6,15 +6,17 @@ const Contact = () => {
       <div className="flex flex-col pt-12 justify-center">
         <p className="stencil text-4xl text-[white] pb-8">Contact Us</p>
         <p className="font-inter text-lg font-semibold pb-20 text-white">
+        
+          <span className="my-4 stencil text-3xl text-black ">Ayush Dev</span> - 93184 85779
+          <br></br>
+          <span className="my-4 stencil text-3xl text-black ">Rakshit Hooda</span> - 98170 38133
+          <br></br>
           <span className="my-4 stencil text-3xl text-black ">Vishu Phogat</span> - 88269 89388, vishu.34343@gmail.com
           <br></br>
           <span className="my-4 stencil text-3xl text-black ">Pawan Choudhary</span> - 90539 95493, Thepawankumar2003@gmail.com
           <br></br>
           <span className="my-4 stencil text-3xl text-black ">Rohanaditya Singh Thakur</span> - 98702 55702, rohanadityasinghthakur@gmail.com
-          <br></br>
-          <span className="my-4 stencil text-3xl text-black ">Ayush Dev</span> - 93184 85779
-          <br></br>
-          <span className="my-4 stencil text-3xl text-black ">Rakshit Hooda</span> - 98170 38133
+          
         </p>
 
       </div>
