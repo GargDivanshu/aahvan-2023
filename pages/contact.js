351b96bf6637e2907704519b1f525c9d5fb9deb5
GargDivanshu <divanshu.garg113@gmail.com>
2023-03-28 00:35:24 +0530

    contact us section update

--- pages/contact.js
+++ pages/contact.js
@@ -6,9 +6,11 @@ const Contact = () => {
       <div className="flex flex-col pt-12 justify-center">
         <p className="stencil text-4xl text-[white] pb-8">Contact Us</p>
         <p className="font-inter text-lg font-semibold pb-20 text-white">
-          <span className="my-4 stencil text-3xl text-black ">Aditya </span> - 90575 41946, ady1812sha@gmail.com
+          <span className="my-4 stencil text-3xl text-black ">Vishu Phogat</span> - 88269 89388, vishu.34343@gmail.com
           <br></br>
-          <span className="my-4 stencil text-3xl text-black ">Deepak </span> - 82399 84913, deepak.cse.dtu@gmail.com
+          <span className="my-4 stencil text-3xl text-black ">Pawan Choudhary</span> - 90539 95493, Thepawankumar2003@gmail.com
+          <br></br>
+          <span className="my-4 stencil text-3xl text-black ">Rohanaditya Singh Thakur</span> - 98702 55702, rohanadityasinghthakur@gmail.com
         </p>
 
       </div>
